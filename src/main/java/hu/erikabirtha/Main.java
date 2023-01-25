d46c22c4179281b63e18c0cb357275367a34c133
Erika Birtha <ebirtha@hotmail.com>
2023-01-25 22:32:24 +0100

    basic settings

--- src/main/java/hu/erikabirtha/Main.java
+++ src/main/java/hu/erikabirtha/Main.java
@@ -1,10 +1,20 @@
 package hu.erikabirtha;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        System.out.println("Hello world! Ez a TollSystem starter");
 
-    ViewTollSystem viewTollSystem = new ViewTollSystem();
+        ContollerTollSystem contollerTollSystem = new ContollerTollSystem();
+        ViewTollSystem viewTollSystem = new ViewTollSystem(contollerTollSystem);
+        ViewModelTollSystem viewModelTollSystem = new ViewModelTollSystem("","", 0, new Date(), new Date(), new Date(), "");
+        DisplayTollSystem presenterTollSystem = new PresenterTollSystem(viewTollSystem);
+        VignetteAPI InteractorTollSystem = new InteractorTollSystem() ;
+
+        viewTollSystem.showVehicleMotorwayVignetteHistory();
+        contollerTollSystem.findVehicleByRegistrationNumber(viewModelTollSystem.registrationNumber);
+
+    }
 
 }
